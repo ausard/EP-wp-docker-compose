bb8bd041fb44ab37f6b0f3cab8a7afd4ad34d898
aleksei sabetski <ausard@yandex.ru>
2020-02-28 16:52:10 +0300

    wordpress

--- plan.md
+++ plan.md
@@ -24,3 +24,37 @@
 # https://www.howtoforge.com/tutorial/dockerizing-wordpress-with-nginx-and-php-fpm/
 
 
+This practical task is aimed to get an idea about Docker and to get hands-on experience with Docker and Docker Compose.
+
+The main goal of the task is to deploy a WordPress site as a multi-container application. The solution should meet the following requirements:
+
+1. Three separate containers should be used for the following services:
+
+· NGINX
+
+· PHP-FRM
+
+· MYSQL
+
+2. Database files and site content should be persistent, i.e. keep the site data if the corresponding container is deleted and be reused once the new container is started.
+
+3. Configuration files for the listed services and the WordPress site should be stored in a Git repository. If the files in the repo are updated, the corresponding containers should be updated automatically with the new configuration.
+
+4. Docker Compose should be utilized to build up and run the mentioned containers.
+
+5. Jenkins is recommended to be used to automate the application deployment. Alternatively, any other CI/CD tool of your choice can be used.
+
+The following two jobs should be set up:
+
+a. Job to deploy the WordPress site with the latest WordPress version.
+
+b. Job to update the containers once any configuration changes are committed.
+
+6. Ansible or Bash scripting can be used for automation.
+
+As an extra mile, after the task is completed according to the listed above points, you may want to alter the solution by using pure Ansible instead of Docker Compose.
+
+A brief description of the working environment and the required tools is given in the following table.
+
+Environment description
+
---
+++ wordpress/wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define( 'DB_USER', 'wordpress');
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'wordpress');
+
+/** MySQL hostname */
+define( 'DB_HOST', 'db:3306');
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'e7ef794fd2b672eda5082ed8846b29d8795072fb');
+define( 'SECURE_AUTH_KEY',  'c588ca01f384c96ea2371eebff43c5309364d9c0');
+define( 'LOGGED_IN_KEY',    '7d24d0f3789a95d5b6a5918684444cb3ea143824');
+define( 'NONCE_KEY',        '1183e9bc7c2cf0820491b0bdc8b76ea4a999264e');
+define( 'AUTH_SALT',        '8f00ec285c2a7076810c03d0fb0b894aff2b3b0e');
+define( 'SECURE_AUTH_SALT', 'b9f5ee86da8f64da8fbc9518a075445c3fd52a1e');
+define( 'LOGGED_IN_SALT',   'f1ffac1bb984d7fe133a6906cd6b07ec641fec73');
+define( 'NONCE_SALT',       'ffa3bc2eed9def6ce0ea6e87e31986ba3d5ab9ae');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
