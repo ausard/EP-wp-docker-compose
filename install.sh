9456f94aa54b3234a231e851ea7cf77f973e4278
Aliaksei Sabetski <ausard@yandex.ru>
2020-02-29 23:41:12 +0300

    1

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 curl -o wordpress.tar.gz -fSL "https://wordpress.org/latest.tar.gz";
-tar -xzf wordpress.tar.gz -C /wordpress ; \
+tar -xzf wordpress.tar.gz -C / ; \
 	rm wordpress.tar.gz; 
 cp wp-conf/wp-config.php /wordpress/wp-config.php
